4dc030a47d3317cf380287ce1c3fbb3ac5010a41
王世林 <15021367785@163.com>
2015-11-22 15:46:52 +0800

    新项目的创建

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by Equal on 15/11/22.
+//  Copyright © 2015年 wangshilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo/weibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weibo
+//
+//  Created by Equal on 15/11/22.
+//  Copyright © 2015年 wangshilin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
